222da0305afaea767ee22e1e4aee1b5eabc39cd0
Luca Coraci <luca.coraci@gmail.com>
2023-03-15 16:59:43 +0100

    roma

--- src/main/java/it/delucia/Main.java
+++ src/main/java/it/delucia/Main.java
@@ -3,5 +3,6 @@ package it.delucia;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao Marina!");
+        System.out.println("Saluti da Roma");
     }
 }
