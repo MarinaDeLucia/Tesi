d81789fdbfa08fe8b6870cc447d7167b00d3f3ab
Luca Coraci <luca.coraci@gmail.com>
2023-04-04 00:16:55 +0200

    Test automation 2

--- src/main/java/it/delucia/algo/Greedy.java
+++ src/main/java/it/delucia/algo/Greedy.java
@@ -451,7 +451,7 @@ public class Greedy {
                 List<JobArrival> jobArrivalByStep = ModelLoader.getInstance().getJobArrivalByStep(step-1);
                 if(jobArrivalByStep!=null){
                     System.out.println(" << im going to process a job arrival:");
-                    System.out.println("test automation");
+                    System.out.println("test automation 2");
                     //jobs.addAll(jobArrivalByStep.stream().map(JobArrival::getJob).toList());
                     //add all new jobs to modelLoader
                     ModelLoader.getInstance().addJobs(jobArrivalByStep.stream().map(JobArrival::getJob).toList());
